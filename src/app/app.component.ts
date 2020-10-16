1fc23a2b3d03075b88c1dec0941c0819ed6e27bc
marcelooliveira <mclricardo@gmail.com>
2020-10-16 19:30:59 -0300

    Angular Wijmo FlexGrid is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+// src/app/app.component.ts
+import { Component, enableProdMode, NgModule, ViewChild } from '@angular/core';
+
+// import Wijmo components
+import * as wjcCore from '@grapecity/wijmo';
+import * as wjcGrid from '@grapecity/wijmo.grid';
+import * as wjcXlsx from '@grapecity/wijmo.xlsx';
+import * as wjcGridXlsx from '@grapecity/wijmo.grid.xlsx';
+import { WjGridModule } from '@grapecity/wijmo.angular2.grid';
+import { recentSales } from "../data/data"
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-wijmo-flexgrid';
+  sales: any[];
+  selectedItem: string;
+  
+  // references FlexGrid named 'flex' in the view
+  @ViewChild('flex') flex: wjcGrid.FlexGrid;
+
+  // DataSvc will be passed by derived classes
+  constructor() {
+      this.sales = recentSales;
+  }
+
+  initializeFlexGrid(flexgrid: wjcGrid.FlexGrid) {
+      // sort the data by country
+      let sd = new wjcCore.SortDescription('Country', true);
+      flexgrid.collectionView.sortDescriptions.push(sd);
+      flexgrid.collectionView.currentChanged.addHandler(this._updateCurrentInfo.bind(this));
+      this._updateCurrentInfo();
+  }
+
+  
+  save() {
+    wjcGridXlsx.FlexGridXlsxConverter.saveAsync(this.flex,
+        {
+          includeColumnHeaders: true,
+          includeCellStyles: false,
+          formatItem: null
+        },
+        'FlexGrid.xlsx');
+  }
+
+  load() {
+      let fileInput = <HTMLInputElement>document.getElementById('importFile');
+      if (fileInput.files[0]) {
+          wjcGridXlsx.FlexGridXlsxConverter.loadAsync(this.flex, fileInput.files[0], { includeColumnHeaders: true });
+      }
+  }
+
+  private _updateCurrentInfo() {
+      this.selectedItem = wjcCore.format(
+          'Country: <b>{country}</b>, Sales: <b>{sales:c0}</b> Expenses: <b>{expenses:c0}</b>',
+          this.flex.collectionView.currentItem);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+// import Wijmo modules
+import { WjGridModule } from '@grapecity/wijmo.angular2.grid';
+
+// apply Wijmo license key
+import { setLicenseKey } from '@grapecity/wijmo';
+setLicenseKey('your license key goes here');
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WjGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
